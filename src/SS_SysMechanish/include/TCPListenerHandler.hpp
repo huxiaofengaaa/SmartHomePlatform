4afc8755784028ef6f1dabdce25aa60158f9066a
huxiaofengaaa <huxiaofengaaa@126.com>
2018-07-03 20:44:22 +0800

    增加AsynTCPListenerHandler类，用于侦听TCP链接请求并完成三次握手协议
        目的 ：增加AsynTCPListenerHandler类，用于侦听TCP链接请求并完成三次握手协议
        作者 ：胡晓锋
        日期 ：2018年07月03日
        类型 ：新功能提交
        描述 ：在TCP三次握手协议完成之后，通过回调函数，返回一个客户端连接信息，包含IP，Port和Socket

---
+++ src/SS_SysMechanish/include/TCPListenerHandler.hpp
@@ -0,0 +1,53 @@
+/*
+ * TCPListenerHandler.hpp
+ *
+ *  Created on: 2018Äê7ÔÂ3ÈÕ
+ *      Author: Administrator
+ */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <thread>
+#include <functional>
+#include <memory>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "glog/logging.h"
+
+struct ClientConnectInfo
+{
+	ClientConnectInfo(int p_sock, std::string p_ip, int p_port)
+		: m_sockfd(p_sock), m_ip(p_ip), m_port(p_port)
+	{
+
+	}
+	int m_sockfd;
+	std::string m_ip;
+	int m_port;
+};
+
+class AsynTCPListenerHandler
+{
+public:
+	AsynTCPListenerHandler(std::string p_host, int p_port, std::function<bool(std::shared_ptr<ClientConnectInfo>)> p_callback);
+	virtual ~AsynTCPListenerHandler();
+	bool runTCPListener();
+	void shutdownTCPListener();
+
+private:
+	void mainloop();
+	int createTCPServerSocket(std::string p_host, int p_port, int p_blockNumber);
+
+	const std::string m_host;
+	const int m_port;
+	std::thread m_thread;
+	bool m_threadExitFlag;
+	std::function<bool(std::shared_ptr<ClientConnectInfo>)> m_callback;
+	int m_sockfd;
+};
+
+
---
+++ src/SS_SysMechanish/src/TCPListenerHandler.cpp
@@ -0,0 +1,89 @@
+/*
+ * TCPListenerHandler.cpp
+ *
+ *  Created on: 2018Äê7ÔÂ3ÈÕ
+ *      Author: Administrator
+ */
+
+#include "TCPListenerHandler.hpp"
+
+AsynTCPListenerHandler::AsynTCPListenerHandler(std::string p_host, int p_port,
+		std::function<bool(std::shared_ptr<ClientConnectInfo>)> p_callback)
+	: m_host(p_host), m_port(p_port), m_sockfd(-1), m_threadExitFlag(false), m_callback(p_callback)
+{
+	LOG(INFO) << "construct AsynTCPListenerHandler";
+}
+
+AsynTCPListenerHandler::~AsynTCPListenerHandler()
+{
+	LOG(INFO) << "de-construct AsynTCPListenerHandler";
+}
+
+bool AsynTCPListenerHandler::runTCPListener()
+{
+	m_sockfd = createTCPServerSocket(m_host, m_port, 20);
+	if(m_sockfd < 0)
+	{
+		return false;
+	}
+	std::function<void()> l_threadTask = std::bind(&AsynTCPListenerHandler::mainloop, this);
+	m_thread = std::move(std::thread(l_threadTask));
+	return true;
+}
+
+void AsynTCPListenerHandler::shutdownTCPListener()
+{
+	m_threadExitFlag = true;
+	m_thread.join();
+	if(m_sockfd > 0)
+	{
+		close(m_sockfd);
+		m_sockfd = -1;
+	}
+}
+
+void AsynTCPListenerHandler::mainloop()
+{
+	LOG(INFO) << "AsynTCPListenerHandler main loop start";
+	while(m_threadExitFlag == false)
+	{
+		struct sockaddr_in cli_addr;
+		socklen_t len = sizeof(struct sockaddr_in);
+
+		int l_clisockfd = accept(m_sockfd, (struct sockaddr*)&cli_addr, &len);
+		if(l_clisockfd >= 0 && m_threadExitFlag == false)
+		{
+			int l_port = ntohs(cli_addr.sin_port);
+			std::string l_ip = inet_ntoa(cli_addr.sin_addr);
+			m_callback(std::make_shared<ClientConnectInfo>(l_clisockfd, l_ip, l_port));
+		}
+	}
+	LOG(INFO) << "AsynTCPListenerHandler main loop exit";
+}
+
+int AsynTCPListenerHandler::createTCPServerSocket(std::string p_host, int p_port, int p_blockNumber)
+{
+	int l_socketfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(l_socketfd < 0)
+	{
+		return -1;
+	}
+
+	struct sockaddr_in ser_addr;
+	bzero(&ser_addr, sizeof(struct sockaddr_in));
+	ser_addr.sin_family = AF_INET;
+	ser_addr.sin_port = htons(p_port);
+	ser_addr.sin_addr.s_addr = inet_addr(p_host.c_str());
+	if(0 != bind(l_socketfd, (struct sockaddr *)&ser_addr, sizeof(struct sockaddr_in)))
+	{
+		close(l_socketfd);
+		return -1;
+	}
+
+	if(0 != listen(l_socketfd, p_blockNumber))
+	{
+		close(l_socketfd);
+		return -1;
+	}
+	return l_socketfd;
+}
