5e89d65ce2a460bf0327141a2af01dac2d6a8cb7
huxiaofengaaa <huxiaofengaaa@126.com>
2018-07-03 19:58:56 +0800

    新增加AndlinkDevicehandler处理逻辑，可以处理heartbeat和auth消息；
        目的 ：新增加AndlinkDevicehandler处理逻辑，可以处理heartbeat和auth消息；
        作者 ：胡晓锋
        日期 ：2018年07月02日
        类型 ：新功能提交
        描述 ：新增加AndlinkDevicehandler处理逻辑，可以处理heartbeat和auth消息；

--- src/SS_EventResolve/src/AndlinkDeviceEventHandler.cpp
+++ src/SS_EventResolve/src/AndlinkDeviceEventHandler.cpp
@@ -16,6 +16,8 @@ std::string AndlinkDeviceEventHandler::run(std::shared_ptr<EventTypeUDPClientDat
 
 	struct Interface56_Register_Req l_registerReq;
 	struct Interface56_Online_Req l_onlineReq;
+	struct Interface56_Auth_Req l_authReq;
+	struct Interface56_heartbeat_Req l_heartbeatReq;
 
 	if(true == resolve_if56_register_request_msg(l_rawData, &l_registerReq))
 	{
@@ -26,6 +28,16 @@ std::string AndlinkDeviceEventHandler::run(std::shared_ptr<EventTypeUDPClientDat
 	{
 		struct Interface56_Online_Resp l_onlineResp;
 		return build_online_response_success_msg(l_onlineResp);
+	}
+	else if(true == resolve_if56_auth_request_msg(l_rawData, &l_authReq))
+	{
+		struct Interface56_Auth_Resp l_authResp;
+		return build_auth_response_msg(l_authResp);
+	}
+	else if(true == resolve_if56_heartbeat_request_msg(l_rawData, &l_heartbeatReq))
+	{
+		struct Interface56_heartbeat_Resp l_heartbeatResp;
+		return build_heartbeat_response_msg(l_heartbeatResp);
 	}
 
 	return std::string();
