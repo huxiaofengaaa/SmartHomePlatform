c67e7af2b5c8f3561653879cc9c4de4120389bdc
huxiaofengaaa <huxiaofengaaa@126.com>
2018-07-20 16:46:25 +0800

    修改服务端plugin请求字段key，原来为随机生成32字节，现在为devRND和SN拼接之后计算md5值
        目的 ：修改服务端plugin请求字段key，原来为随机生成32字节，现在为devRND和SN拼接之后计算md5值
        作者 ：胡晓锋
        日期 ：2018年07月20日
        类型 ：新增加功能
        描述 ：客户端需要对该key字段验证，如果计算不对，需要拒绝plugin请求

--- src/SS_EventResolve/src/AndlinkDeviceEventBuilder.cpp
+++ src/SS_EventResolve/src/AndlinkDeviceEventBuilder.cpp
@@ -6,6 +6,7 @@
  */
 #include "AndlinkDeviceEventBuilder.hpp"
 #include "RandomGenerator.hpp"
+#include "MD5.h"
 
 std::string AndlinkDeviceEventBuilder::buildPlugIuRequest(std::string p_deviceid,
 		std::string p_host, int p_port)
@@ -19,7 +20,15 @@ std::string AndlinkDeviceEventBuilder::buildPlugIuRequest(std::string p_deviceid
 	struct Interface56_RequestPlugIn_Req req;
 	req.ServerAddr = p_host;
 	req.ServerPort = std::to_string(p_port);
-	req.Key = l_generator.generatorRandomNumberString(32);
+
+	std::string l_devSN = l_uecontext->deviceSn;
+	std::string l_devRND = l_uecontext->DevRND;
+	char l_md5Result[64] = { 0 };
+	char l_srcString[256] = { 0 };
+	snprintf(l_srcString, sizeof(l_srcString), "%s%s", l_devRND.c_str(), l_devSN.c_str());
+	Compute_string_md5(l_srcString, l_md5Result);
+
+	req.Key = std::string(l_md5Result);
 	return build_if56_requestPlugIn_request_msg(req);
 }
 
